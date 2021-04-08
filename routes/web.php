bc93e155621c6052a679db4efb27980c137c4e1f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-08 15:49:44 +0100

    Used home controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::view('/', 'home.index')
+Route::get('/', [HomeController::class, 'home'])
     ->name('home.index');
 
-Route::view('/contact', 'home.contact')
+Route::get('/contact', [HomeController::class, 'contact'])
     ->name('home.contact');
 
 $posts = [
