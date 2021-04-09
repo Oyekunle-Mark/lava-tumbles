a136df2f369912f8aa93cfe2eaa14c15f8fc674c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-09 20:10:17 +0100

    Added functional test

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testHomePage()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response->assertSeeText('Hello, Laravel!');
+    }
+
+    public function testContactPage()
+    {
+        $response = $this->get('/contact');
+
+        $response->assertSeeText('Contact Page');
+    }
+}
