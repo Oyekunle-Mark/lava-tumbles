58f81ac7755f0037798adbbc8659e5620b412b82
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-10 19:51:03 +0100

    show comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', $post['title'])
 
 @section('content')
-@if($post['is_new'])
+{{--  @if($post['is_new'])
 <div>A new blog post. Using if</div>
 @else
 <div>Blog post is old. Using elseif</div>
@@ -11,13 +11,18 @@
 
 @unless ($post['is_new'])
     <div>This is an old blog post... using unless</div>
-@endunless
+@endunless  --}}
 
-<h1>{{ $post['title'] }}</h1>
-<p> {{ $post['content'] }}</p>
+<h1>{{ $post->title }}</h1>
+<p> {{ $post->content }}</p>
 
-@isset($post['has_comments'])
-    <div>The post has some comments... using isset</div>
-@endisset
+<h4>Comments</h4>
 
+@forelse ($post->comments as $comment)
+    <p>
+        {{ $comment->content }}
+    </p>
+@empty
+    <p>No comments yet.</p>
+@endforelse
 @endsection
