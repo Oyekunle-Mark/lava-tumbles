a98ba30d2b8b5b39dd43c0d0aa570ad29c31b16a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-13 14:54:13 +0100

    created seeders

---
+++ database/seeders/BlogPostsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BlogPostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
