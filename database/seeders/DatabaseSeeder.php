c903c794f948c45f7dfab6b67e68860e2ea76793
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-13 14:51:06 +0100

    Created seeders in the comment run function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory()->state([
+        $user_doe = \App\Models\User::factory()->state([
             'name' => 'John Doe',
             'email' => 'jd@test.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         ])->create();
-        \App\Models\User::factory(20)->create();
+        $else = \App\Models\User::factory(20)->create();
+
+        $users = $else->concat([$user_doe]);
+
+        $posts = \App\Models\BlogPost::factory()
+            ->count(50)
+            ->make()
+            ->each(function ($post) use ($users) {
+                $post->user_id = $users->random()->id;
+                $post->save();
+            });
+
+        $comments = \App\Models\Comment::factory()
+            ->count(150)
+            ->make()
+            ->each(function ($comment) use ($posts) {
+                $comment->blog_post_id = $posts->random()->id;
+                $comment->save();
+            });
     }
 }
