168dc03a630b1ecb48ca02e1594259f4d24e249c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-13 14:38:10 +0100

    used factory in seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory()->state([
             'name' => 'John Doe',
             'email' => 'jd@test.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         ])->create();
         \App\Models\User::factory(20)->create();
     }
