aa005a316b87965316e3911a0afb5cdac0f96aa2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-13 22:05:17 +0100

    conditionally render edit and delete buttons

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -8,15 +8,20 @@
 @endif
 
 <div>
-    <a href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
-    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <input type="submit" value="Delete!">
-        @if ($post->comments_count)
-            <p>{{ $post->comments_count }} comments</p>
-        @else
-            <p>No comments yet!</p>
-        @endif
-    </form>
+    @can('update', $post)
+        <a href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
+    @endcan
+
+    @can('delete', $post)
+        <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="Delete!">
+            @if ($post->comments_count)
+                <p>{{ $post->comments_count }} comments</p>
+            @else
+                <p>No comments yet!</p>
+            @endif
+        </form>
+    @endcan
 </div>
