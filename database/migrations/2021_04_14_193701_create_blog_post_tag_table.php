72e6d6df000d447451d8c8abe39749927eb9ecf3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-14 20:40:22 +0100

    created migration for blog posts tag table

---
+++ database/migrations/2021_04_14_193701_create_blog_post_tag_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_post_tag', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('blog_post_id')->unsigned()->index();
+            $table->bigInteger('tag_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('blog_post_id')
+                ->references('id')
+                ->on('blog_posts')
+                ->onDelete('cascade');
+            $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_post_tag');
+    }
+}
