7e9ae2a57442018361fdc8d56cb68d21e4662a9c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-14 15:23:49 +0100

    used a global scope

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -20,7 +20,10 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('posts.index', ['posts' => BlogPost::withCount('comments')->get()]);
+        return view(
+            'posts.index',
+            ['posts' => BlogPost::withCount('comments')->get()]
+        );
     }
 
     /**
