5874ad8448d510edd3521ec6a082797f77a62d85
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-15 19:37:44 +0100

    created ppolymorphic relationship in the model

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,11 @@ class User extends Authenticatable
         return $this->hasMany(Comment::class);
     }
 
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
     public function scopeWithMostBlogPost(Builder $query)
     {
         return $query->withCount('blogPosts')->orderBy('blog_posts_count', 'desc');
