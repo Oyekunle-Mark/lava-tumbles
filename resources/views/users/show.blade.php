6c85955e1a13370e0f4f656805ac9d6003384f5a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-15 20:52:04 +0100

    showed comments on user

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,4 +7,39 @@
     <div>
         <p>{{ $user->name }}</p>
     </div>
+
+    @auth
+        <form action="{{ route('users.comments.store', ['user' => $user->id]) }}" method="POST">
+            @csrf
+            <div>
+                <textarea type="text" name="content"></textarea>
+            </div>
+            <div>
+                <input type="submit" value="Add Comment"/>
+            </div>
+
+            @if($errors->any())
+                <div>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </form>
+    @else
+        <a href="{{ route('login') }}">Sign in to post comment</a>
+    @endauth
+
+    <hr />
+
+    @forelse ($user->commentsOn as $comment)
+        <p>
+            {{ $comment->content }}
+        </p>
+        <span>Created by {{ $comment->user->name }}</span>
+    @empty
+        <p>No comments yet.</p>
+    @endforelse
 @endsection
