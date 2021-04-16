eff22b1e158b98bd51cfde442d857f99e2c196c6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-16 18:16:53 +0100

    rendered email previews in the browser

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\PostsController;
 use App\Http\Controllers\PostTagController;
 use App\Http\Controllers\UserCommentController;
 use App\Http\Controllers\UserController;
+use App\Mail\CommentPostedMarkdown;
+use App\Models\Comment;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -118,5 +120,10 @@ Route::resource('users', UserController::class)
 
 Route::resource('users.comments', UserCommentController::class)
     ->only(['store']);
+
+Route::get('mailable', function () {
+    $comment = Comment::find(1);
+    return new CommentPostedMarkdown($comment);
+});
 
 Auth::routes();
