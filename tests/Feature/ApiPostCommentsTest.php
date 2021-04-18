39e0de4eae7bc284c28cac615abbc1b55561275b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-18 20:24:07 +0100

    tested api get method

---
+++ tests/Feature/ApiPostCommentsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\BlogPost;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ApiPostCommentsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testNewBlogPostDoesNotHaveComment()
+    {
+        BlogPost::factory()->create([
+            'user_id' => $this->user()->id,
+        ]);
+
+        $response = $this->json('GET', 'api/v1/posts/1/comments');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data', 'links', 'meta'])
+            ->assertJsonCount(0, 'data');
+    }
+}
