7e17e0afb369cb71b4ac62ce16ddbb2d3cfe1c69
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-18 16:33:28 +0100

    Added a new api resource in a new namespace

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,27 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')
+    ->name('api.v1.')
+    ->namespace('Api\V1')
+    ->group(function () {
+        Route::get('/status', function () {
+            return response()->json(['status' => 'OK']);
+        })->name('status');
+
+        Route::apiResource('posts.comments', 'PostCommentController');
+    });
+
+Route::prefix('v2')
+    ->name('api.v2.')
+    ->group(function () {
+        Route::get('/status', function () {
+            return response()->json(['status' => true]);
+        })->name('status');
+    });
