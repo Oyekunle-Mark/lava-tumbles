b1c0216531b9e9058c6f601fa6fdd2b2aaf4f4c5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-04-18 17:18:43 +0100

    Added options to the pagination

--- app/Http/Controllers/Api/V1/PostCommentController.php
+++ app/Http/Controllers/Api/V1/PostCommentController.php
@@ -14,10 +14,17 @@ class PostCommentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(BlogPost $post)
+    public function index(BlogPost $post, Request $request)
     {
+        $perPage = $request->input('per_page') ?? 15;
+
         return CommentResource::collection(
-            $post->comments()->with('user')->paginate(),
+            $post->comments()
+                ->with('user')
+                ->paginate($perPage)
+                ->appends([
+                    'per_page' => $perPage,
+                ]),
         );
     }
 
